fdeba6b8c0e6f612944a35efc6057229f66d6971
Samnejad92 <m.samnejad92@gmail.com>
2024-09-11 21:28:36 +0330

    validate length and validate zeroes method created

--- Program.cs
+++ Program.cs
@@ -9,16 +9,36 @@ then ipAddress is valid
 
 else ipAddress is invalid
 */
+string ipv4Input = "107.31.1.5";
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
+void ValidateLength() 
+{
+    string[] address = ipv4Input.Split(".");
+    validLength = address.Length == 4;
+};
+void ValidateZeroes() 
+{
+    string[] address = ipv4Input.Split(".");
 
-if (ValidateLength() && ValidateZeroes() && ValidateRange()) 
+foreach (string number in address) 
+{
+    if (number.Length > 1 && number.StartsWith("0")) 
+    {
+        validZeroes = false;
+        return;
+    }
+}
+    validZeroes = true;
+}
+ValidateRange();
+
+if (validLength && validZeroes && validRange) 
 {
     Console.WriteLine($"ip is a valid IPv4 address");
 } 
-else 
+else
 {
     Console.WriteLine($"ip is an invalid IPv4 address");
 }
-
-void ValidateLength() {}
-void ValidateZeroes() {}
-void ValidateRange() {}
