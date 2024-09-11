373cf7021e8f6cfe14ffb100fc0111764b60ecc0
Samnejad92 <m.samnejad92@gmail.com>
2024-09-11 21:39:56 +0330

    finished prroject

--- Program.cs
+++ Program.cs
@@ -1,40 +1,48 @@
-﻿/*
-if ipAddress consists of 4 numbers
-and
-if each ipAddress number has no leading zeroes
-and
-if each ipAddress number is in range 0 - 255
-
-then ipAddress is valid
-
-else ipAddress is invalid
-*/
-string ipv4Input = "107.31.1.5";
+﻿string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255"};
+string[] address;
 bool validLength = false;
 bool validZeroes = false;
 bool validRange = false;
-void ValidateLength() 
-{
-    string[] address = ipv4Input.Split(".");
-    validLength = address.Length == 4;
-};
-void ValidateZeroes() 
-{
-    string[] address = ipv4Input.Split(".");
 
-foreach (string number in address) 
+foreach (string ip in ipv4Input) 
 {
-    if (number.Length > 1 && number.StartsWith("0")) 
+    address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+    ValidateLength(); 
+    ValidateZeroes(); 
+    ValidateRange();
+
+    if (validLength && validZeroes && validRange) 
     {
-        validZeroes = false;
-        return;
+        Console.WriteLine($"{ip} is a valid IPv4 address");
+    } 
+    else 
+    {
+        Console.WriteLine($"{ip} is an invalid IPv4 address");
     }
 }
+
+void ValidateLength() 
+{
+    validLength = address.Length == 4;
+};
+
+void ValidateZeroes() 
+{
+    foreach (string number in address) 
+    {
+        if (number.Length > 1 && number.StartsWith("0")) 
+        {
+            validZeroes = false;
+            return;
+        }
+    }
+
     validZeroes = true;
 }
+
 void ValidateRange() 
 {
-    string[] address = ipv4Input.Split(".", StringSplitOptions.RemoveEmptyEntries);
     foreach (string number in address) 
     {
         int value = int.Parse(number);
@@ -45,12 +53,4 @@ void ValidateRange()
         }
     }
     validRange = true;
-}
-if (validLength && validZeroes && validRange) 
-{
-    Console.WriteLine($"ip is a valid IPv4 address");
-} 
-else
-{
-    Console.WriteLine($"ip is an invalid IPv4 address");
 }
